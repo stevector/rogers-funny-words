b74554903f7c3bbf7978382ce582045a3c0767a1
Steve Persch <steve.persch@pantheon.io>
2020-02-25 16:22:49 -0600

    frontity

---
+++ frontity/frontity.settings.js
@@ -0,0 +1,41 @@
+const settings = {
+  "name": "frontity",
+  "state": {
+    "frontity": {
+      "url": "https://test.frontity.io",
+      "title": "Test Frontity Blog",
+      "description": "WordPress installation for Frontity development"
+    }
+  },
+  "packages": [
+    {
+      "name": "@frontity/twentytwenty-theme",
+      "state": {
+        "theme": {
+          "menu": [
+            [
+              "Home",
+              "/"
+            ]
+          ],
+          "featured": {
+            "showOnList": false,
+            "showOnPost": false
+          }
+        }
+      }
+    },
+    {
+      "name": "@frontity/wp-source",
+      "state": {
+        "source": {
+          "api": "https://test-rogers-funny-words.pantheonsite.io/wp-json"
+        }
+      }
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react"
+  ]
+};
+
+export default settings;
