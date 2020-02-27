a3014d7ca59c54ebf0cf6dddba3e7116ab1afd13
Steve Persch <steve.persch@pantheon.io>
2020-02-27 15:25:55 -0600

    env vars

--- frontity/frontity.settings.js
+++ frontity/frontity.settings.js
@@ -1,3 +1,8 @@
+const env = process.env.TERMINUS_ENV;
+const site = process.env.TERMINUS_SITE;
+const url = `https://${env}-${site}.pantheonsite.io`;
+const api = `${url}/wp-json`;
+
 const settings = {
   "name": "frontity",
   "state": {
@@ -29,7 +34,7 @@ const settings = {
       "name": "@frontity/wp-source",
       "state": {
         "source": {
-          "api": "https://test-rogers-funny-words.pantheonsite.io/wp-json"
+          "api": api
         }
       }
     },
